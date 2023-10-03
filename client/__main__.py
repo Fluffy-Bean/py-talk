fd7be1586c1a277e3da822c86b19ecd01e82b615
Fluffy-Bean <michal-gdula@protonmail.com>
2023-10-03 15:09:06 +0100

    Add tkinter UI for client yoinked from somewhere else temporarily

--- client/__main__.py
+++ client/__main__.py
@@ -1,15 +1,22 @@
-import os
 import socket
 import threading
 import json
 from datetime import datetime
 import time
+import tkinter as tk
 
 
-HEADER = 512
+HEADER_SIZE = 512
 PORT = 8080
 HOST = socket.gethostbyname(socket.gethostname())
 FORMAT = "UTF-8"
+VERSION = "0.0.2"
+STYLING = {
+    "bg": "#17202A",
+    "fg": "#EAECEE",
+    "font": "Helvetica 13 bold",
+    "padding": 5,
+}
 
 
 class Client:
@@ -21,91 +28,196 @@ class Client:
         self.username = "User"
 
         self._client = socket.socket()
-        self._online = False
 
-    def run(self):
-        self.username = input("Username: ").strip()
+        # Create Window
+        self.window = tk.Tk()
+        self.window.withdraw()
 
-        join_data = {
-            "username": self.username,
-            "version": "0.0.1",
-        }
-        join_data = json.dumps(join_data).encode()
+        # Login root
+        self._login = tk.Toplevel()
+        self._login.title("Login")
+        self._login.resizable(width=False, height=False)
+        self._login.configure(width=400, height=300)
 
-        while not self._online:
+        # Label for login
+        self._title_text = tk.Label(
+            self._login,
+            text="Please login to continue",
+            justify=tk.CENTER,
+            font=STYLING["font"],
+        )
+        self._title_text.place(relheight=0.15, relx=0.2, rely=0.07)
+
+        # Label and entry for username
+        self._login_label = tk.Label(self._login, text="Name: ", font=STYLING["font"])
+        self._login_label.place(relheight=0.2, relx=0.1, rely=0.2)
+        self._login_entry = tk.Entry(self._login, font=STYLING["font"])
+        self._login_entry.place(relwidth=0.4, relheight=0.12, relx=0.35, rely=0.2)
+        self._login_entry.focus()
+
+        # Button to continue
+        login_continue = tk.Button(
+            self._login,
+            text="CONTINUE",
+            font=STYLING["font"],
+            command=lambda: self._main_loop(self._login_entry.get()),
+        )
+        login_continue.place(relx=0.4, rely=0.55)
+
+        self.window.mainloop()
+
+    def _main_loop(self, name):
+        self.username = name.strip()
+
+        online = False
+        while not online:
             try:
                 self._client.connect((self.host, self.port))
-                self._client.send(join_data)
+                self._client.recv(HEADER_SIZE).decode()
             except ConnectionRefusedError:
                 print("Server is not available. Retrying in 2 seconds...")
                 time.sleep(2)
             else:
-                self._online = True
+                online = True
+
+        self._client.send(
+            json.dumps(
+                {
+                    "username": self.username,
+                    "version": VERSION,
+                }
+            ).encode()
+        )
+        self._login.destroy()
+        self._layout()
 
         thread = threading.Thread(target=self._receive_message)
         thread.start()
-
-        try:
-            self.refresh()
-
-            while True:
-                message = input().strip()
-                if not message:
-                    continue
-
-                message = {
-                    "content": message,
-                    "time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
-                    "username": self.username,
-                    "version": "0.0.1",
-                }
-                message = json.dumps(message).encode()
-
-                try:
-                    self._client.send(message)
-                except BrokenPipeError:
-                    print("Gone offline")
-                    break
-        except KeyboardInterrupt:
-            print("Client shutting down...")
-        finally:
-            thread.join()
-            self._client.close()
 
     def _receive_message(self):
         system_message = {
             "content": "Connected to server!",
             "time": datetime.now().strftime("%H:%M:%S"),
             "username": "System",
-            "version": "0.0.1",
+            "version": VERSION,
         }
         self.messages.append(system_message)
+        self._append_message(system_message)
 
         while True:
-            content = self._client.recv(HEADER).decode()
+            content = self._client.recv(HEADER_SIZE).decode()
             content = json.loads(content.strip())
-
-            try:
-                message = {
+            self.messages.append(
+                {
                     "content": content["content"],
                     "time": content["time"],
                     "username": content["username"],
                     "version": content["version"],
                 }
-            except KeyError:
-                print("Invalid data received!")
+            )
+            self._append_message(content)
+
+    def _append_message(self, message):
+        message = f"{message['username']} [{message['time']}]: {message['content']} (version {message['version']})"
+        self.textCons.config(state=tk.NORMAL)
+        self.textCons.insert(tk.END, message + "\n\n")
+        self.textCons.config(state=tk.DISABLED)
+        self.textCons.see(tk.END)
+
+    def _layout(self):
+        self.window.deiconify()
+        self.window.title("PyChat v" + VERSION + " - Logged in as: " + self.username)
+        self.window.resizable(width=False, height=False)
+        self.window.configure(width=470, height=550, bg="#17202A")
+
+        self.label_heading = tk.Label(
+            self.window,
+            bg="#17202A",
+            fg="#EAECEE",
+            text="PyChat v" + VERSION,
+            font=STYLING["font"],
+            pady=5,
+        )
+        self.label_heading.place(relwidth=1)
+
+        self.line = tk.Label(self.window, width=450, bg="#ABB2B9")
+        self.line.place(relwidth=1, rely=0.07, relheight=0.012)
+
+        self.textCons = tk.Text(
+            self.window,
+            width=20,
+            height=2,
+            bg="#17202A",
+            fg="#EAECEE",
+            font=STYLING["font"],
+            padx=5,
+            pady=5,
+        )
+        self.textCons.place(relheight=0.745, relwidth=1, rely=0.08)
+
+        self.labelBottom = tk.Label(self.window, bg="#ABB2B9", height=80)
+        self.labelBottom.place(relwidth=1, rely=0.825)
+
+        self.entryMsg = tk.Entry(
+            self.labelBottom,
+            bg="#2C3E50",
+            fg="#EAECEE",
+            font=STYLING["font"],
+        )
+        self.entryMsg.place(relwidth=0.74, relheight=0.06, rely=0.008, relx=0.011)
+        self.entryMsg.focus()
+
+        self.buttonMsg = tk.Button(
+            self.labelBottom,
+            text="Send",
+            font=STYLING["font"],
+            width=20,
+            bg="#ABB2B9",
+            command=lambda: self._send_button(self.entryMsg.get()),
+        )
+        self.buttonMsg.place(relx=0.77, rely=0.008, relheight=0.06, relwidth=0.22)
+
+        self.textCons.config(cursor="arrow")
+
+        scrollbar = tk.Scrollbar(self.textCons)
+        scrollbar.place(relheight=1, relx=0.974)
+        scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=tk.DISABLED)
+
+    def _send_button(self, msg):
+        self.textCons.config(state=tk.DISABLED)
+        self.msg = msg
+        self.entryMsg.delete(0, tk.END)
+        snd = threading.Thread(target=self._send_message)
+        snd.start()
+
+    # function to send messages
+    def _send_message(self):
+        self.textCons.config(state=tk.DISABLED)
+
+        while True:
+            message = self.msg.strip()
+            print(message)
+
+            if not message:
                 continue
 
-            self.messages.append(message)
-            self.refresh()
+            message = {
+                "content": message,
+                "time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                "username": self.username,
+                "version": VERSION,
+            }
+            message = json.dumps(message).encode()
 
-    def refresh(self):
-        os.system("clear")
-        for message in self.messages:
-            print(f"{message['username']} ({message['time']}): {message['content']}")
-        print(end="")
+            try:
+                self._client.send(message)
+            except BrokenPipeError:
+                print("Gone offline")
+
+            break
 
 
 if __name__ == "__main__":
     client = Client(HOST, PORT)
-    client.run()
--- server/__main__.py
+++ server/__main__.py
@@ -10,45 +10,40 @@ HOST = socket.gethostbyname(socket.gethostname())
 FORMAT = "UTF-8"
 
 
-"""
-example message contents looks as follows:
-{
-    "content": "Hello World!",  This is the actual message content
-    "time": "2021-01-01 00:00:00",  This is the time the message was sent in UTC
-    "username": "user",  This is the username of the user who sent the message, only temporary
-    "version": "0.0.1"  This is the version of the client that sent the message, can be used for compatibility in the future
-}
-"""
-
-
 class SocketChat:
     def __init__(self, host="localhost", port=8080):
-        self.host = host
-        self.port = port
+        self.host, self.port = host, port
 
-        self._server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._server.bind((self.host, self.port))
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.bind((host, port))
 
-        self._clients = {}
-        self._messages = []
+        self.clients = {}
+        self.messages = []
 
     def run(self):
-        self._server.listen()
+        self.server.listen()
         print("Server is listening on %s:%d" % (self.host, self.port))
 
         while True:
-            client, address = self._server.accept()
+            client, address = self.server.accept()
+            client.send("pong".encode())
 
-            thread = threading.Thread(target=self._client, args=(client, address))
+            initial_data = client.recv(HEADER).decode()
+            initial_data = json.loads(initial_data.strip())
+            self.clients[client] = {
+                "username": initial_data["username"],
+                "address": address,
+                "version": initial_data["version"],
+            }
+
+            self.broadcast("New user joined the chat!")
+
+            thread = threading.Thread(target=self.handle, args=(client, address))
             thread.start()
 
             print("Active connections:", str(threading.active_count() - 1))
 
-            self._broadcast("New user joined the chat!")
-
-        self._server.close()
-
-    def _broadcast(self, message, username="Server"):
+    def broadcast(self, message, username="Server"):
         message = {
             "content": message,
             "time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
@@ -57,14 +52,14 @@ class SocketChat:
         }
         message = json.dumps(message).encode()
 
-        for client in self._clients.copy():
+        for client in self.clients.copy():
             try:
                 client.send(message)
             except BrokenPipeError:
-                self._remove_client(client)
+                self.remove_client(client)
 
-    def _direct(self, message, client):
-        client = self._clients[client]
+    def direct(self, message, client):
+        client = self.clients[client]
 
         message = {
             "content": message,
@@ -77,51 +72,40 @@ class SocketChat:
         try:
             client.send(message)
         except BrokenPipeError:
-            self._remove_client(client)
+            self.remove_client(client)
 
-    def _client(self, client, address):
-        initial_data = client.recv(HEADER).decode()
+    def handle(self, client, address):
+        client_data = self.clients[client]
+        connected = True
 
-        print(initial_data)
-
-        initial_data = json.loads(initial_data.strip())
-
-        client_data = None
-        try:
-            client_data = {
-                "username": initial_data["username"],
-                "address": address,
-                "version": initial_data["version"],
-            }
-            self._clients[client] = client_data
-        except KeyError:
-            client.send("Invalid data!".encode())
-            client.close()
-            return
-        finally:
-            print("New connection from %s:%d" % (address[0], address[1]))
-
-        while True:
+        while connected:
             message = client.recv(HEADER).decode()
-
-            print(message)
-
+            if not message:
+                break
             message = json.loads(message.strip())
-
-            if not message or not message["content"]:
+            if not message["content"]:
+                client.send(
+                    json.dumps(
+                        {
+                            "content": "Malformed message!",
+                            "time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S"),
+                            "username": "Server",
+                            "version": "0.0.1",
+                        }
+                    ).encode()
+                )
                 continue
 
-            self._messages.append(message)
-            self._broadcast(message["content"], client_data["username"])
+            self.messages.append(message)
+            self.broadcast(message["content"], client_data["username"])
 
-    def _remove_client(self, client):
-        try:
-            self._broadcast(str(self._clients[client]["username"]) + " left the chat!")
-            self._clients.pop(client)
-            client.close()
-        except Exception as e:
-            print(e)
-            # There is a problem with the code here, but I don't know what it is so trying to sus it out
+        self.remove_client(client)
+        client.close()
+
+    def remove_client(self, client):
+        self.broadcast(str(self.clients[client]["username"]) + " left the chat!")
+        self.clients.pop(client)
+        client.close()
 
 
 if __name__ == "__main__":
